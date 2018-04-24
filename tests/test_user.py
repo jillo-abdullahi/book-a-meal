e096c4a991276323a742329a0cfe2561a31578c0
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-04-24 19:25:05 +0300

    [Fix] push set up variables out of the set up method

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,7 +8,7 @@ class TestUserLogin(unittest.TestCase):
 		"""Creates the app as a test client"""
 		app.testing = True
 		self.app = app.test_client()
-
+		
 	def test_successful_registration(self):
 		response = self.register_user()
 		self.assertEqual(response.status_code, 201)
@@ -16,21 +16,30 @@ class TestUserLogin(unittest.TestCase):
 
 
 	def test_successful_login(self):
-		self.register_user()
 		response = self.login_user()
 		self.assertEqual(response.status_code, 200)
 		output = json.loads(response.get_data(as_text=True))['message']
 		self.assertEqual(output, 'Login success')
 
 	def register_user(self):
+		new_user_info = {
+            "username":"zayn",
+            "full-name":"Zayn Malik",
+            "email":"jayloabdullahi@gmail.com",
+            "password":"check1234"
+            }
 		response = self.app.post('/api/v1/auth/register',
-			data = json.dumps(self.new_user_info),
+			data = json.dumps(new_user_info),
 			content_type='application/json')
 		return response
 
 	def login_user(self):
+		user_login_info = {
+			"username":"zayn",
+			"password":"check1234"
+			}
 		response = self.app.post('/api/v1/auth/register',
-			data = json.dumps(self.user_login_info),
+			data = json.dumps(user_login_info),
 			content_type='application/json')
 		return response
 
