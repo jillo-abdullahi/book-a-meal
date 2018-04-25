c97994427200076f1af4a77733e7e658bccc75fd
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-04-25 18:24:07 +0300

    [Fix] Update __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,35 @@
-from flask import Flask
+from flask_api import FlaskAPI
+from flask import request, jsonify
 
+from instance.config import app_config
 
-app = Flask(__name__)
+all_users = []
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    
+    
+
+    @app.route("/api/v1/auth/register", methods=["POST"])
+    def register_new_user():
+    	get_user = request.get_json()
+    	all_users.append(get_user)
+
+    	return jsonify({"message":all_users}),201
+
+    @app.route('/api/v1/auth/login', methods=["POST"])
+    def login_user():
+    	get_user = request.get_json()
+
+    	username = get_user["username"]
+    	password = get_user["password"]
+
+    	for user in all_users:
+    		if username == user["username"] and password == user["password"]:
+				return jsonify({"user":user,"message":"User authenticated"}),200
+
+		return jsonify({"message": "User was not found"}),404
+
+    return app
