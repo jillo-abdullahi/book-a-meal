376d821d30ddd13cabc4f7149d47219168f995e1
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-04-30 20:39:50 +0300

    [Chore] Remove db settings for now

--- instance/config.py
+++ instance/config.py
@@ -1,26 +1,30 @@
 import os
 
+
 class Config(object):
-	"""Configuration for parent class"""
-	DEBUG = False
-	CSRF_ENABLED = True
-	SECRET = os.getenv('SECRET')
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    """Configuration for parent class"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
     DEBUG = True
+
 
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
