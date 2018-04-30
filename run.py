b807bc19a0c728de4218af32e0359b07e8d6787d
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-04-30 20:37:41 +0300

    [Chore] Remove debug mode

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
+config_name = os.getenv('APP_SETTINGS')  # config_name = "testing"
 app = create_app(config_name)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
