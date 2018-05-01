cf1bf67317c002627840748198eeffc553474542
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-05-01 18:26:34 +0300

    [Fix] Convert JSON response from bytes

--- tests/test_manage_meals.py
+++ tests/test_manage_meals.py
@@ -106,7 +106,7 @@ class TestManageMeals(unittest.TestCase):
                                        data=json.dumps(user_login_info),
                                        content_type='application/json')
 
-        access_token = json.loads(login_response.data)['access_token']
+        access_token = json.loads(login_response.data)["access_token"]
 
         headers = dict(Authorization="Bearer {}".format(access_token))
         return headers['Authorization']
