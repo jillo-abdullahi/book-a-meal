9e40700993f6083306c64f4ae60ae083325972cb
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-05-07 00:24:29 +0300

    [Feature] Add ability to create super admin at the beginning

--- manage.py
+++ manage.py
@@ -3,13 +3,34 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app.models import modelsV2
+from app.models.modelsV2 import User, Meals, Menu, Orders
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def createsuperadmin():
+    """Create a super admin first"""
+    username = os.getenv('DEFAULT_ADMIN_USERNAME')
+    # password = os.getenv('DEFAULT_ADMIN_PASSWORD')
+    # full_name = os.getenv('DEFAULT_ADMIN_FULL_NAME')
+    # admin = os.getenv('DEFAULT_ADMIN_ROLE')
+    # email = os.getenv('DEFAULT_ADMIN_EMAIL')
+
+    username = "super-admin"
+    password = "password"
+    full_name = "super super"
+    admin = True
+    email = "admin@book-a-meal.com"
+
+    user = User(username=username, password=password,
+                full_name=full_name, admin=admin, email=email)
+    user.save()
+    print("Message: Admin Created")
 
 
 if __name__ == '__main__':
