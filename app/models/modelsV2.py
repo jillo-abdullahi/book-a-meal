f3a23293dc5e74c111514f377c6d4549a9a6fe74
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-05-06 16:33:43 +0300

    [Fix] Switch to default table names

--- app/models/modelsV2.py
+++ app/models/modelsV2.py
@@ -5,8 +5,6 @@ from app import db
 class User(db.Model):
     """Class representing users"""
 
-    __tablename__ = 'user'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     full_name = db.Column(db.String(50))
